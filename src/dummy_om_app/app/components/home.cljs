d14df5760902e1e8de91a70c6dc2a772b3f3350b
Pat Thomas <pthomas@relaynetwork.com>
2015-01-10 13:17:23 -0500

    Home view text.

--- src/dummy_om_app/app/components/home.cljs
+++ src/dummy_om_app/app/components/home.cljs
@@ -6,6 +6,23 @@
             [om.dom                 :as dom :include-macros true])
   (:require-macros [om-utils.core :refer [defcomponent]]))
 
+(def home-text
+  [{:render-fn dom/div
+    :text      "Welcome to the app!"}
+   {:render-fn dom/div
+    :text      "It doesn't do much yet... but you can read about the technologies I used to build it below."}
+   {:render-fn dom/a
+    :text      "Om (client side)"
+    :js-opts   #js {:href   "https://github.com/swannodette/om"
+                    :target "_none"}}
+   {:render-fn dom/a
+    :text      "Sham (server side)"
+    :js-opts   #js {:href   "https://github.com/pat-thomas/sham"
+                    :target "_none"}}])
+
 (defcomponent home
   (render
-   (dom/div nil "home view will go here")))
+   (apply dom/div nil
+          (map (fn [{:keys [render-fn text js-opts]}]
+                 (dom/div nil (render-fn js-opts text)))
+               home-text))))
