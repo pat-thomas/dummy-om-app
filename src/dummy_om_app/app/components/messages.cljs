234afb645f28a9a30937c630f8a5e96d65d170b7
Pat Thomas <pthomas@relaynetwork.com>
2015-01-10 00:37:53 -0500

    Basic rendering of messages.

--- src/dummy_om_app/app/components/messages.cljs
+++ src/dummy_om_app/app/components/messages.cljs
@@ -1,11 +1,22 @@
 (ns dummy-om-app.app.components.messages
   (:require [dummy-om-app.app.state :as app-state]
+            [dummy-om-app.app.models.messages :as messages-model]
             [dummy-om-app.app.xhr   :as xhr]
             [dummy-om-app.app.util  :as util]
             [om.core                :as om  :include-macros true]
             [om.dom                 :as dom :include-macros true])
   (:require-macros [om-utils.core :refer [defcomponent]]))
 
-(defcomponent messages
+(defcomponent message-detail
   (render
-   (dom/div nil "messages will go here")))
+   (dom/div #js {:className "message-detail"}
+            (str "Sender: " (get data "sender_id") ", " (get data "content")))))
+
+(defcomponent messages
+  (will-mount
+   (messages-model/fetch))
+  (render
+   (apply dom/div #js {:id "messages"}
+          (map (fn [message-data]
+                 (om/build message-detail message-data))
+               (get-in data [:users :messages])))))
