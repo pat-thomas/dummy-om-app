5d178590823e18da59ad07ef102c1db50dbfd518
Pat Thomas <pthomas@relaynetwork.com>
2015-01-16 22:59:27 -0500

    Basic session model code.

---
+++ src/dummy_om_app/app/models/session.cljs
@@ -0,0 +1,22 @@
+(ns dummy-om-app.app.models.session
+  (:require [dummy-om-app.app.util  :as util]
+            [dummy-om-app.app.state :as app-state]
+            [dummy-om-app.app.xhr   :as xhr]))
+
+(defn get-session
+  []
+  (xhr/xhr-req {:method :get
+                :url    "session"
+                :on-complete (fn [resp]
+                               (swap! app-state/app-state assoc :session resp))}))
+
+(defn dispatch-on-session-status
+  [session-dispatch-table & [data]]
+  (xhr/xhr-req
+   {:method      :get
+    :url         "session"
+    :on-complete (fn [{:keys [status] :as resp}]
+                   (when-let [handler (get session-dispatch-table status)]
+                     (if data
+                       (handler data)
+                       (handler))))}))
