f34a8bebbc8f5778a3ffb4ab56b553d02c495873
Pat Thomas <pthomas@relaynetwork.com>
2015-01-19 12:57:51 -0500

    Convert xhr-req to use core.async.

--- src/dummy_om_app/app/xhr.cljs
+++ src/dummy_om_app/app/xhr.cljs
@@ -1,6 +1,8 @@
 (ns dummy-om-app.app.xhr
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [goog.events           :as events]
-            [dummy-om-app.app.util :as util])
+            [dummy-om-app.app.util :as util]
+            [cljs.core.async       :as async])
   (:import [goog.net XhrIo]
            goog.net.EventType
            [goog.events EventType]))
@@ -15,10 +17,12 @@
 
 (defn xhr-req
   [{:keys [method url data on-complete]}]
-  (let [xhr (XhrIo.)]
+  (let [xhr      (XhrIo.)
+        xhr-chan (async/chan 1)]
     (events/listen xhr goog.net.EventType.COMPLETE
                    (fn [e]
-                     (on-complete (util/parse-json (.getResponseText xhr)))))
-    (. xhr
-       (send (str api-base-url url) (meths method) (when data (util/write-json data))
-             #js {"Content-Type" "application/json"}))))
+                     (async/put! xhr-chan (on-complete (util/parse-json (.getResponseText xhr))))))
+    (go (. xhr
+           (send (str api-base-url url) (meths method) (when data (util/write-json data))
+                 #js {"Content-Type" "application/json"}))
+        (async/<! xhr-chan))))
